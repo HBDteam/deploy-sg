6b0f30653421a86f0ffcdfd8d02ae9d993219b7a
imchaehee <dla3090@sungkyul.ac.kr>
2022-03-29 19:29:30 +0900

    admin_main, sign models

--- sign/models.py
+++ sign/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
 class User(models.Model):
-    studentID = models.CharField(max_length = 8, primary_key = True)
-    password = models.BinaryField(max_length = 16, null = False)
-    name = models.CharField(max_length = 10, null = False )
-    email = mpdelsEmailField(null = False)
-    phoneNum = models.CharField(max_length = 11, null = False)
-    isEnrolled = models.BooleanField(default = 0, null = False)
+    studentID = models.CharField(max_length=8, primary_key=True, null=False)
+    password = models.BinaryField(max_length=16, null=False)
+    name = models.CharField(max_length=10, null=False)
+    email = models.EmailField(null=False)
+    phoneNum = models.CharField(max_length=11, null=False)
+    isEnrolled = models.BooleanField(default=0, null=False)
+
 
 class Manager(models.Model):
-    mID =  models.CharField(max_length = 8, primary_key = True)
-    mPassword = models.BinaryField(max_length = 16, null = False)
+    mID = models.CharField(max_length=8, primary_key=True, null=False)
+    mPassword = models.BinaryField(max_length=16, null=False)
