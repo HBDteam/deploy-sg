dd4a56084a71216632d10d1042b1a7f6e815d387
gilyeon <12venus08@naver.com>
2022-04-13 23:29:11 +0900

    add to aichatbot files & edit user models.py

--- aichatbot/models.py
+++ aichatbot/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+# 챗봇 데이터 학습용 테이블
+
+class ChatTrain(models.Model):
+    id = models.AutoField(primary_key=True, null = False)
+    intent = models.CharField(max_length=45)
+    ner = models.CharField(max_length=1024)
+    query = models.TextField(default='')
+    answer = models.TextField(null = False)
+    answer_image = models.CharField(max_length=2048)
--- user_main/models.py
+++ user_main/models.py
@@ -5,13 +5,13 @@ from admin_main.models import Equip, EquipCode
 
 
 class Lab(models.Model):
-    labNum = models.IntegerField(max_length=3, primary_key=True, null = False)
+    labNum = models.IntegerField(primary_key=True, null = False)
     manager = models.CharField(max_length=10, null=False)
     mPhoneNum = models.CharField(max_length=11, null=False)
-    curPeople = models.IntegerField(max_length=2, null=False)
+    curPeople = models.IntegerField(null=False)
 
 class Renting(models.Model):
-    serialnum = models.IntegerField(max_length=4, primary_key=True, null = False)
+    serialnum = models.IntegerField(primary_key=True, null = False)
     equipID = models.ForeignKey(Equip, on_delete=models.PROTECT, null = False)
     userName = models.CharField(max_length=10, null=False)
     userID = models.ForeignKey(User, on_delete=models.PROTECT, null = False)
