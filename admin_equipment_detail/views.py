bbefb1fcba9569ef08c911af508f292dcb059206
imchaehee <dla3090@sungkyul.ac.kr>
2022-05-08 19:44:34 +0900

    관리자 메인과 장비상세목록페이지 db연동,기능구현

--- admin_equipment_detail/views.py
+++ admin_equipment_detail/views.py
@@ -1,3 +1,12 @@
+from django.contrib import admin
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Equip, EquipCode
 
 # Create your views here.
+
+
+def adminEquipDetail(request):
+    equips = Equip.objects.all()
+    context = {'equips': equips}
+    return render(request, 'admin_equipment_detail.html', context)
--- admin_main/models.py
+++ admin_main/models.py
@@ -1,6 +1,5 @@
 from statistics import mode
 from django.db import models
-from user_main.models import *
 from sign.models import *
 
 # 장비 코드(카테고리) 테이블
@@ -10,14 +9,20 @@ class EquipCode(models.Model):
     equipCode = models.IntegerField(primary_key=True, null=False)
     equipName = models.CharField(max_length=20, null=False)
 
+    def __str__(self):
+        return self.equipName
+
 
 # 각 장비의 정보 테이블
 
 
 class Equip(models.Model):
-    euipCode = models.ForeignKey(EquipCode, on_delete=models.DO_NOTHING)
-    equipID = models.CharField(primary_key=True, max_length=10, null=False)
+    equipCode = models.ForeignKey(EquipCode, on_delete=models.DO_NOTHING)
+    equipID = models.CharField(primary_key=True, max_length=13, null=False)
     receivedDate = models.DateField(default='')
     isRented = models.PositiveSmallIntegerField(null=False)
     status = models.PositiveSmallIntegerField(null=False)
     equipInfo = models.TextField(default='')
+
+    def __str__(self):
+        return self.equipID
