1fb2b3427696fa09cbae5e7b62ac03e9a47aa931
jeonga <wjddk4182@gmail.com>
2022-05-14 19:54:10 +0900

    urls

--- success/urls.py
+++ success/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
     path('sign/', include('sign.urls')),
     path('admin_equipment/', include('admin_equipment.urls')),
