0b532970f78938e1df8397768c0fc7114ceed9ad
yujeongnfd <shr2008@sungkyul.ac.kr>
2022-05-21 16:47:12 +0900

    user_history, user_main

--- user_history/views.py
+++ user_history/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from email.encoders import encode_quopri
 
-# Create your views here.
+from admin_main.models import EquipCode
+from user_equipment.views import equipinfo
+from .models import User, Renting, Equip
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+#
+
+
+
+def user_history(request):
+    
+    NOW_userID = Renting.objects.filter(userID="20190811")
+   
+
+    context = {'NOW_userID' : NOW_userID}
+    
+    
+    return render(request, 'user_history.html', context)
