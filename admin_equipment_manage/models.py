ee719738af813fd21ee21fb93b4e27193fcebb7d
imchaehee <dla3090@sungkyul.ac.kr>
2022-05-24 14:54:32 +0900

    관리자 장비 등록, 수정 페이지 완성

--- admin_equipment_manage/models.py
+++ admin_equipment_manage/models.py
@@ -1,3 +1,3 @@
 from django.db import models
-
+from admin_main.models import *
 # Create your models here.
--- user_main/models.py
+++ user_main/models.py
@@ -5,22 +5,22 @@ from admin_main.models import *
 
 # Create your models here.
 
+
 class Lab(models.Model):
-    labNum = models.IntegerField(max_length=3, primary_key=True, null = False)
+    labNum = models.IntegerField(primary_key=True, null=False)
     manager = models.CharField(max_length=10, null=False)
     mPhoneNum = models.CharField(max_length=11, null=False)
-    curPeople = models.IntegerField(max_length=2, null=False)
+    curPeople = models.IntegerField(null=False)
+
 
 class Renting(models.Model):
-    serialnum = models.IntegerField(max_length=4, primary_key=True, null = False)
-    equipID = models.ForeignKey(Equip, on_delete=models.PROTECT, null = True)
+    serialnum = models.IntegerField(primary_key=True, null=False)
+    equipID = models.ForeignKey(Equip, on_delete=models.PROTECT, null=True)
     userName = models.CharField(max_length=10, null=True)
-    userID = models.ForeignKey(User, on_delete=models.PROTECT, null = True)
+    userID = models.ForeignKey(User, on_delete=models.PROTECT, null=True)
     userPhoneNum = models.CharField(max_length=11, null=True)
     rentingDate = models.DateField(null=True)
     returningDate = models.DateField(null=True)
 
     def __str__(self):
         return f'[{self.serialnum}]'
-
-    
