bdb74debcfec7316a9cfc1c1e7d2c69a54052882
Gilyeon <52391627+gilyeon00@users.noreply.github.com>
2022-09-07 22:01:17 +0900

    Feat : add success admin urls

--- success/urls.py
+++ success/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     
     path('admin/', admin.site.urls),
-
+    path('sign/', include('sign.urls')),
+    path('admin_main/', include('admin_main.urls')),
+    path('user_main/',include('user_main.urls'))
 ]
