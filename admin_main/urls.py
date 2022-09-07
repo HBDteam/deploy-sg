8828a3d69f906172cd1f822800de28e6384fa976
Gilyeon <52391627+gilyeon00@users.noreply.github.com>
2022-09-07 22:02:28 +0900

    Feat : add admin_main urls

--- admin_main/urls.py
+++ admin_main/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.adminMain)
+    path('', views.adminMain),
+    path('admin_renting/', views.renting),
+    path('rent_detail_ajax/', views.rent_detail_ajax)
 ]
