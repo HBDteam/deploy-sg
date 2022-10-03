5c9a05f07762bcb97f2823d4e3579ba1fbdc8b9a
gilyeon00 <12venus08@naver.com>
2022-10-03 18:09:29 +0900

    Refactor : #2 폴더 구조로 변동으로 인한 경로 변경 - 테스트완료

--- success/urls.py
+++ success/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     
     path('admin/', admin.site.urls),
     path('sign/', include('sign.urls')),
+    path('aichatbot/', include('aichatbot.urls')),
     path('feat_admin/admin_main/', include('feat_admin.admin_main.urls')),
     path('feat_user/user_main/',include('feat_user.user_main.urls')),
     path('feat_user/user_equipment/',include('feat_user.user_equipment.urls')),
@@ -14,6 +15,9 @@ urlpatterns = [
     path('feat_admin/admin_equipment/', include('feat_admin.admin_equipment.urls')),
     path('feat_admin/admin_equipment_manage/', include('feat_admin.admin_equipment_manage.urls')),
     path('feat_admin/admin_equipment_detail/', include('feat_admin.admin_equipment_detail.urls')),
-    path('feat_admin/admin_equipment_history/', include('feat_admin.admin_equipment_history.urls'))
-
+    path('feat_admin/admin_equipment_history/', include('feat_admin.admin_equipment_history.urls')),
+    path('feat_user/user_main/', include('feat_user.user_main.urls')),
+    path('feat_user/user_equipment/', include('feat_user.user_equipment.urls')),
+    path('feat_user/user_history/', include('feat_user.user_history.urls')),
+    path('feat_user/user_rental/', include('feat_user.user_rental.urls'))
 ]
