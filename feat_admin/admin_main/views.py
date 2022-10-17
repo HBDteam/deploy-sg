cc33cab81feaffba12f24b665d9eed988dd39eec
jeonga0303 <wjddk4182@gmail.com>
2022-10-17 21:49:41 +0900

    대여처리 수정

--- feat_admin/admin_main/views.py
+++ feat_admin/admin_main/views.py
@@ -22,17 +22,20 @@ def adminMain(request):
 
 def renting(request):
     if request.method == 'GET':
-        now_date = datetime.datetime.now(timezone("Asia/Seoul")) - datetime.timedelta(days=7) # 오늘 날짜를 가져온다. (test: 7일전)
-        user_history_query = Renting.objects.filter().only('userName','equipID', 'rentingDate', 'returningDate') # Renting 테이블에서 원하는 컬럼만 가져온다.
+        # now_date = datetime.datetime.now(timezone("Asia/Seoul")) - datetime.timedelta(days=7) # 오늘 날짜를 가져온다. (test: 7일전)
+        user_history_query = Renting.objects.filter().only('userName','equipID_no', 'rentingDate', 'returningDate') # Renting 테이블에서 원하는 컬럼만 가져온다.
         rent_user_list = []
+        rent_list = []
         for data in user_history_query:
-            if data.rentingDate.strftime("%Y-%m-%d") >= now_date.strftime("%Y-%m-%d") : # 오늘 날짜 이후인 것만 보여준다.
-                rent_user_list.append(data)
+            # if data.rentingDate.strftime("%Y-%m-%d") >= now_date.strftime("%Y-%m-%d") : # 오늘 날짜 이후인 것만 보여준다.
+            rent_user_list.append(data)
         for data in rent_user_list:
-            equip_query = Equip.objects.get(equipID__contains=data.equipID)
-            if str(data.equipID) == str(equip_query.equipID):
-                data.equipInfo = equip_query.equipInfo
-        context = {'rent_user_list' : rent_user_list }
+            equip_query = Equip.objects.filter(equipID=data.equipID_no)
+            if equip_query:
+                rent_list.append(data)
+                if str(data.equipID_no) == str(equip_query[0].equipID):
+                    data.equipInfo = equip_query[0].equipInfo
+        context = {'rent_user_list' : rent_list }
         return render(request, 'admin_equipment_rental.html', context)
     # return render(request, 'admin_equipment_rental.html')
 
@@ -43,8 +46,8 @@ def rent_detail_ajax(request):
         select_userName = request.POST.get('select_userName')
         select_rentingDate = request.POST.get('select_rentingDate')
         select_returningDate = request.POST.get('select_returningDate')
-        user_rented_query = Renting.objects.get(userName=select_userName,equipID=select_equipID) #유저이름과 장비코드를 사용해 유저 아이디를 찾는다.
-        select_equipID = user_rented_query.userID_id
+        user_rented_query = Renting.objects.get(userName=select_userName,equipID_no=select_equipID) #유저이름과 장비코드를 사용해 유저 아이디를 찾는다.
+        select_equipID = user_rented_query.userID_no
         user_detail_query = User.objects.get(studentID__contains=select_equipID)
         print(user_detail_query)
         studentID = user_detail_query.studentID
