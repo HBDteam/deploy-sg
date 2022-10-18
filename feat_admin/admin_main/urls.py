962b6904900e82466459858ec826aef33bc72c09
imchaehee <dla3090@sungkyul.ac.kr>
2022-10-18 13:44:08 +0900

    반납처리 수정

--- feat_admin/admin_main/urls.py
+++ feat_admin/admin_main/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.adminMain),
     path('admin_renting/', views.renting),
-    path('rent_detail_ajax/', views.rent_detail_ajax)
+    path('rent_detail_ajax/', views.rent_detail_ajax),
+    path('admin_returning/', views.returning),
+    path('return_detail_ajax/', views.return_detail_ajax)
 ]
--- feat_admin/admin_main/views.py
+++ feat_admin/admin_main/views.py
@@ -66,7 +66,7 @@ def rent_detail_ajax(request):
         return JsonResponse({'studentID': studentID, 'studentName': studentName, 'studentEmail': studentEmail, 'studentPhonenum': studentPhonenum})
 
 
-def renturning(request):
+def returning(request):
     if request.method == 'GET':
         # now_date = datetime.datetime.now(timezone("Asia/Seoul")) - datetime.timedelta(days=7) # 오늘 날짜를 가져온다. (test: 7일전)
         user_history_query = Renting.objects.filter().only('userName', 'equipID_no',
@@ -109,3 +109,27 @@ def return_detail_ajax(request):
         print(Equip_q.values())
 
         return JsonResponse({'studentID': studentID, 'studentName': studentName, 'studentEmail': studentEmail, 'studentPhonenum': studentPhonenum})
+
+
+# *@csrf_exempt
+# def return_ajax(request):
+#   equipid = request.POST['select_equipID']
+#    equips = Equip.objects.filter(equipID=equipid)
+#    for equip in equips:
+#        if request.POST.get('equipCode') is None:
+#            equip.equipID = request.POST['equipID']
+#            equip.receivedDate = request.POST['receivedDate']
+#            equip.isRented = request.POST['isRented']
+#            equip.status = request.POST['status']
+#            equip.equipInfo = request.POST['equipInfo']
+#            equip.save()
+#        else:
+#            equip.equipCode = request.POST['equipCode']
+#            equip.equipID = request.POST['equipID']
+#            equip.receivedDate = request.POST['receivedDate']
+#            equip.isRented = request.POST['isRented']
+#            equip.status = request.POST['status']
+#            equip.equipInfo = request.POST['equipInfo']
+#            equip.save()
+#    return redirect('http://127.0.0.1:8000/admin_equipment_detail/', {'alert': '수정되었습니다'})
+#
