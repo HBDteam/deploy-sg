2427a20d6717a7f5f7ddc420b1258056b875b80b
gilyeon00 <12venus08@naver.com>
2022-10-23 23:46:04 +0900

    #3 Feat : Ai 환경세팅 및 schema 변경

--- aichatbot/models.py
+++ aichatbot/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 class ChatTrain(models.Model):
     id = models.AutoField(primary_key=True, null = False)   # 학습 데이터 id
-    intent = models.CharField(max_length=45 )    # 의도명 의도가 없는 경우 null EX 실습장비 대여, 실습실 예약 등/
+    intent = models.CharField(max_length=45,  null = True, default='' )    # 의도명 의도가 없는 경우 null EX 실습장비 대여, 실습실 예약 등/
     ner = models.CharField(max_length=1024, null = True,default='')     # 개체명 개체가 없는 경우 null
     message = models.TextField(default='')        # 질문 텍스트
     answer = models.TextField(null = False)     # 답변 텍스트
--- success/settings.py
+++ success/settings.py
@@ -109,9 +109,9 @@ USE_I18N = True
 USE_TZ = False
 
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, '/static'),
 ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
