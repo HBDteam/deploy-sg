de4b24d5c4dbcccabe2832e344b4b98785d50d06
gilyeon00 <12venus08@naver.com>
2022-11-04 04:04:50 +0900

    default url sign으로 변경

--- success/urls.py
+++ success/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     
     path('admin/', admin.site.urls),
-    path('sign/', include('sign.urls')),
+    path('', include('sign.urls')),
     path('aichatbot/', include('aichatbot.urls')),
     path('feat_admin/admin_main/', include('feat_admin.admin_main.urls')),
     path('feat_user/user_main/',include('feat_user.user_main.urls')),
