2221523063c6e8dd37e19a03de9f085322231d2b
gilyeon00 <12venus08@naver.com>
2022-11-04 12:06:55 +0900

    user rental 오류 수정

--- feat_user/user_rental/views.py
+++ feat_user/user_rental/views.py
@@ -6,31 +6,34 @@ from .import views
 
 #신청서 작성
 def rental(request):
-    login_user_ID = request.user.username
-    Users = User.objects.filter(studentID=login_user_ID)
-    post = Renting()
-    post.userPhoneNum = Users.values('phoneNum')
-    post.userName = Users.values('name')
-    post.userID_no = Users.values('studentID')
+    token_userID= request.user.username
+    rent_Users = User.objects.filter(studentID=token_userID)
+    rentpost = Renting()
+    rentpost.userPhoneNum = rent_Users.values('phoneNum')
+    rentpost.userName = rent_Users.values('name')
+    rentpost.userID_no = rent_Users.values('studentID')
     
 
     if(request.method == "GET"):
-        
+
         equipid = request.GET.get('equipid')
         pr_equipids = Equip.objects.filter(equipID = equipid) 
-        context = {'Users' : Users, 'pr_equipids': pr_equipids }
+        context = {'Users' : rent_Users, 'pr_equipids': pr_equipids }
         return render(request, 'user_rental.html', context)
 
     if(request.method == "POST"):     
         #post.userID = request.POST['userid']  
-        post.rentingDate = request.POST['rentdate']
-        post.returningDate = request.POST['returndate']
-        post.equipID_no = request.POST.get("equipids", "")
+        rentpost.rentingDate = request.POST['rentdate']
+        rentpost.returningDate = request.POST['returndate']
+        rentpost.equipID_no = request.POST.get("equipids", "")
         #post.returningDate = request.POST['equipid']
         #print(a)
         #post.userID = request.POST['studentID']
-        
-        
-        post.save()   
 
-        return redirect('../equipinfo', {'alert':'정상적으로 신청이 접수되었습니다'})
+        
+        rentpost.userID = None
+        rentpost.equipID = None
+        
+        rentpost.save()   
+
+        return redirect('../user_equipment', {'alert':'정상적으로 신청이 접수되었습니다'})
